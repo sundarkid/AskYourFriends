20a4183152fb7e43dacf44775accafae2fdd2d0d
sundarkid <sundareswaran25@gmail.com>
2015-05-03 01:50:49 +0530

    Completed Signup web module.
    Added Gist:
    https://gist.github.com/016985038bd5c36a49b7.git

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+
 $salt = "jj%asd@f&83!";
 $connect = mysqli_connect("mysql3.000webhost.com","a8267023_akurfnd","connect2me","a8267023_akurfnd");
 
@@ -18,5 +19,31 @@ $phone = $_POST['phone'];
 $place = $_POST['place'];
 $institution = $_POST['institution'];
 $password = $_POST['password'];
+$password = md5($password.$salt);
+$ccode = md5(uniqid(rand()));
+
+// SQL query for entering into Table
+$query = "INSERT INTO `temp_user` (`name`,`institution`,`place`,`phone`,`mail_id`,`time`,`ccode`,`password`) VALUES ('$name','$institution','$place','$phone','$mail_id','$dat','$ccode','$password')";
+
+$result = mysqli_query($connect,$query);
+
+// Sending mail and server response
+if($result)
+{
+	$subject="Ask Your Friends, Comfirmation mail ";
+	$header="from: Sundareswaran <sundareswarancg@gmail.com>";
+	$message="Your Comfirmation link \r\n";
+	$message.="Click on this link to activate your account \r\n";
+	$message.="http://www.askyourfriends.site40.net/confirm.php?passkey=$ccode";// The Activation link
+	$sent_mail = mail($mail_id,$subject,$message,$header); // Sending Email
+	
+	$a = array('result' => "Success");
+	echo json_encode($a);
+}
+else
+{
+	$a = array('result' => "Failure");
+		echo json_encode($a);
+}
 
 ?>
