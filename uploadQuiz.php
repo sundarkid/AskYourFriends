c23caa51dfd41d59b0fd4bf39f76050fd606d825
sundarkid <sundareswaran25@gmail.com>
2015-05-14 20:43:14 +0530

    Added Upload Questions..

---
+++ uploadQuiz.php
@@ -0,0 +1,38 @@
+<?php
+$connect=mysqli_connect("mysql3.000webhost.com","a8267023_akurfnd","connect2me","a8267023_akurfnd");
+	if (mysqli_connect_errno()) 
+	{
+  			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+
+
+// Date and time
+date_default_timezone_set('Asia/Kolkata');
+$dat= date("F j, Y, g:i a"); 
+
+// Variables and values 
+$user_id = $_POST['user_id'];
+$data = stripcslashes($_POST['quiz_data']);
+$friends_to_send = stripcslashes($_POST['friends']);
+$arr = json_decode($data,true);
+
+// Logging the activity to get the grouping id
+$query_loging = "INSERT INTO `new_questions_log` ( `user_id` , `friends_to_send` , `time`) VALUES ('$user_id' , '$friends_to_send' , '$dat' )";
+$result1 = mysqli_query($connect,$query_loging);
+if($result1){
+	$query_getting_groupid = "SELECT `group_id` FROM `new_questions_log` WHERE `time`='$dat'";
+	$result2 = mysqli_query($connect,$query_getting_groupid);
+	if($result2){
+		$row = mysqli_fetch_array($result2);
+		$grouping_id = $row['group_id'];
+		$query_notify_new_info = "INSERT INTO `new_question_info` (`user_id` , `grouping_id` , `friends_to_send`) VALUES ( '$user_id', '$grouping_id' , '$friends_to_send');"
+		$result3 = mysqli_query($connect,$query_notify_new_info);
+		foreach($arr as $val=>$k){
+			$query="INSERT  INTO `new_questions`(`user_id`,`grouping_id`,`question`,`optionA`,`optionB`,`optionC`,`optionD`,`answer`) VALUES ('$user_id', '$grouping_id' ,".$k['question']."','".$k['opt1']."','".$k['opt2']."','".$k['opt3']."','".$k['opt4']."','".$k['ans']."')";
+			$result4=mysqli_query($connect,$query);
+		}
+	}
+}
+	mysqli_close($connect);
+	
+?>
